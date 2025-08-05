b2e3ce0532e2af340e927cd4e0d1240af6c73394
Deepak Shukla <flamesofwisdom004@gmail.com>
2025-08-05 12:06:20 +0530

    using shadcn and its controls

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h2>Welcome to the Home Page</h2>
+      <Button variant="destructive">Click Me</Button>
     </div>
   );
 }
